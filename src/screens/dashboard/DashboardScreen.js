140d8cd855221d7a093ca06a517cdc1fdb24bb7f
Jorge Manzano <manzanoglez1995@gmail.com>
2020-09-12 16:52:52 -0500

    Arreglando la master

--- src/screens/dashboard/DashboardScreen.js
+++ src/screens/dashboard/DashboardScreen.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const DashboardScreen = () => {
     return (
         <div>
-            <h1>Dashboard EN NUEVA RAMA</h1>
+            <h1>Dashboard Nuevo Master modificado en empezarDash</h1>
         </div>
     );
 }
